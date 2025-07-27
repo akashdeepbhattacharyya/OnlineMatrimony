223b525fbb3fcb1ecf1a2e79157d05e1f6ff2b9e
Pallab Maiti <pallab.maiti@everest.engineering>
2025-07-27 16:44:15 +0530

    Add splash image and environment configuration sample
    - Added a new splash image to the assets.
    - Created a sample environment configuration file (envrc.sample) with placeholders for various environment variables.
    - Refactored the useHttpClient function for improved readability and consistency in formatting.

--- src/api/useHttpClient.ts
+++ src/api/useHttpClient.ts
@@ -1,9 +1,10 @@
+import { IS_LOCAL } from '@/app.config';
 import { HttpClient } from '@/src/api/HttpClient';
+import Constants from 'expo-constants';
 
-export function useHttpClient(defaultHeaders: Record<string, string>, token?: string) {
-  return new HttpClient(
-    process.env.API_BASE_URL || '',
-    defaultHeaders,
-    token,
-  );
+export function useHttpClient(
+  defaultHeaders: Record<string, string>,
+  token?: string,
+) {
+  return new HttpClient(process.env.API_BASE_URL || '', defaultHeaders, token);
 }
