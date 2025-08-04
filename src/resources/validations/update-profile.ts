b905f7e9bd1711071bcc61999c64f25a9bc9e4a6
Pallab Maiti <pallab.maiti@everest.engineering>
2025-08-04 18:11:12 +0530

    feat: update user profile validation schema to include additional fields for comprehensive user information

--- src/resources/validations/update-profile.ts
+++ src/resources/validations/update-profile.ts
@@ -2,6 +2,13 @@ import * as Yup from 'yup';
 import { genders } from '../gender';
 import { cities, stateCityMapping, states } from '../city-state';
 import { UpdateUserProfileFormType } from '../form';
+import { maritalStatuses } from '../marital-status';
+import { religions } from '../religion';
+import { castes } from '../caste';
+import { diets } from '../diet';
+import { educations } from '../education';
+import { motherTongues } from '../mother-tongue';
+import { occupations } from '../occupation';
 
 export const updateUserProfileSchema = Yup.object<UpdateUserProfileFormType>({
   fullName: Yup.string().required('Full Name is required'),
@@ -9,14 +16,6 @@ export const updateUserProfileSchema = Yup.object<UpdateUserProfileFormType>({
   gender: Yup.string()
     .oneOf(Object.keys(genders))
     .required('Gender is required'),
-  // height: Yup.number()
-  //   .min(0, 'Height must be positive')
-  //   .required('Height is required'),
-  // weight: Yup.number()
-  //   .min(0, 'Weight must be positive')
-  //   .required('Weight is required'),
-  // maritalStatus: Yup.string().oneOf(Object.keys(maritalStatus)),
-  // religion: Yup.string().oneOf(Object.keys(religions)),
   state: Yup.string().oneOf(Object.keys(states)),
   city: Yup.string().when('state', (state, schema) => {
     if (
@@ -42,4 +41,30 @@ export const updateUserProfileSchema = Yup.object<UpdateUserProfileFormType>({
       otherwise: schema => schema,
     }),
   aboutMe: Yup.string(),
+  height: Yup.number()
+    .min(0, 'Height must be positive')
+    .required('Height is required'),
+  weight: Yup.number()
+    .min(0, 'Weight must be positive')
+    .required('Weight is required'),
+  maritalStatus: Yup.string()
+    .oneOf(Object.keys(maritalStatuses))
+    .required('Marital Status is required'),
+  religion: Yup.string()
+    .oneOf(Object.keys(religions))
+    .required('Religion is required'),
+  caste: Yup.string().oneOf(Object.keys(castes)).required('Caste is required'),
+  motherTongue: Yup.string()
+    .oneOf(Object.keys(motherTongues))
+    .required('Mother Tongue is required'),
+  diet: Yup.string().oneOf(Object.keys(diets)).required('Diet is required'),
+  education: Yup.string()
+    .oneOf(Object.keys(educations))
+    .required('Education is required'),
+  occupation: Yup.string()
+    .oneOf(Object.keys(occupations))
+    .required('Occupation is required'),
+  annualIncome: Yup.string()
+    .matches(/^\d+$/, 'Annual Income must be a valid number')
+    .required('Annual Income is required'),
 });
